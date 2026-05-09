1d827689bbac61bd4942f3f2c1093240a68e13c6
sijanxt <sijanbartaula39@gmail.com>
2026-05-09 21:56:01 +0545

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Hero from "@/components/home/hero"
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+    </main>
+  )
+}
---
+++ components/home/hero.tsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+  return (
+    <div className="relative isolate overflow-hidden bg-gray-900">
+        <img    className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
+                src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+                alt="Image of a beach during sunset" />
+    </div>
+  )
+}
